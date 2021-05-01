4860671837f8ded5d7d95537000a533e915bc873
Luke Conibear <laconibear@gmail.com>
2021-05-01 18:25:25 +0100

    test

--- myapp.py
+++ myapp.py
@@ -1,35 +1,19 @@
-import pandas as pd
-import geopandas as gpd
-import json
-from bokeh.models import GeoJSONDataSource 
-from bokeh.plotting import figure, curdoc
 from bokeh.layouts import column
- 
- 
-# Read the country borders shapefile into python using Geopandas 
-shapefile = 'data/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp'
-gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry']]
+from bokeh.plotting import figure, curdoc
 
-# Rename the columns
-gdf.columns = ['country', 'country_code', 'geometry']
- 
+# prepare some data
+x = [1, 2, 3, 4, 5]
+y1 = [6, 7, 2, 4, 5]
+y2 = [2, 3, 4, 5, 6]
+y3 = [4, 5, 5, 7, 2]
 
-# Convert the GeoDataFrame to GeoJSON format so it can be read by Bokeh
-merged_json = json.loads(gdf.to_json())
-json_data = json.dumps(merged_json)
-geosource = GeoJSONDataSource(geojson=json_data)
+# create a new plot with a title and axis labels
+p = figure(title="Multiple line example", x_axis_label="x", y_axis_label="y")
 
+# add multiple renderers
+p.line(x, y1, legend_label="Temp.", line_color="blue", line_width=2)
+p.line(x, y2, legend_label="Rate", line_color="red", line_width=2)
+p.line(x, y3, legend_label="Objects", line_color="green", line_width=2)
 
-# Make the plot
-TOOLTIPS = [
-('UN country', '@country')
-]
-
-p = figure(title='World Map', plot_height=600 , plot_width=950, tooltips=TOOLTIPS,
-x_axis_label='Longitude', y_axis_label='Latitude')
-
-p.patches('xs','ys', source=geosource, fill_color='white', line_color='black',
-hover_fill_color='lightblue', hover_line_color='black')
- 
-# This final command is required to launch the plot in the browser
+# show the results
 curdoc().add_root(column(p))
